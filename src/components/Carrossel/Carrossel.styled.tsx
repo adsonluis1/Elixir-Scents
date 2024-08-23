abe09a07d52b6b10574c36ea4a7ba076f66cbb53
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-23 12:52:52 -0300

    components de estilo do component carrossel

---
+++ src/components/Carrossel/Carrossel.styled.tsx
@@ -0,0 +1,60 @@
+import styled, { css } from "styled-components";
+
+export const Section = styled.section`
+    width: 100vw;
+    height: 150px;
+`
+
+export const Title = styled.h2`
+    margin:20px;
+`
+
+export const DivWrapper = styled.div`
+    overflow-x: auto;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+    &::-webkit-scrollbar{
+        display: none;
+    }
+`
+
+export const DivImg = styled.div`
+    display: flex;
+    justify-content: baseline;
+    align-items: center;
+    height: 100%;
+    gap: 1vw;
+    flex-flow:row nowrap;
+`
+
+type ArrowBtn = {
+    $position:"left" | "right"
+}
+
+export const ArrowBtn = styled.div<ArrowBtn>`
+    transition: 0.2s;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    cursor: pointer;
+    color: white;
+    ${({$position})=> $position == "left"?
+        css`
+            left: 0px;
+        `     
+        :
+        css`
+            right: 0px;
+        `
+    }
+    background-color: #00000045;
+    height: 150px;
+    width: 20px;
+
+    &:hover{
+        background-color:#0000009d;
+        width: 30px;
+    }
+`
