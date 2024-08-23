2bb0d9ec5c56379eb337f0f0318e274cd5588246
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-23 12:59:31 -0300

    rota dinamica das marcas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Register from "./pages/register/Register"
 import { Route, Routes } from "react-router-dom"
 import DashbordLoginRegister from "./pages/dashbordLoginRegister/DashbordLoginRegister"
 import Home from "./pages/home/Home"
+import Perfumes from "./pages/Perfumes/Perfumes"
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
         <Route path="/dashbordAccount" element={<DashbordLoginRegister />}/>
+        <Route path="/:marca" element={<Perfumes />}/>
       </Routes>
     </>
   )
