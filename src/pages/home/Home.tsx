db54634a192fbe715d93d6064b47de286f752d14
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-23 12:57:31 -0300

    add as vitrines das principais marcas

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,32 @@
 import * as S from './Home.styled'
 import Header from '../../components/header/Header'
-import MaisVendidos from '../../components/maisVendidos/MaisVendidos'
+import Carrossel from '../../components/Carrossel/Carrossel'
+import Propaganda from '../../components/Propaganda/Propaganda'
 
-const Home = () => {
+const Home = () => {  
   return (
     <>
         <Header />
         <S.Main>
-            <MaisVendidos />
+            <Carrossel title={"Mais vendidos"}>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+                <S.Img src='/img/login.webp'></S.Img>
+            </Carrossel>
+          <S.Section>
+            <Propaganda reverse={false} title={'Chanel'} sr={"Chanel"}/>
+            <Propaganda reverse={true} title='Dior' sr='Dior'/>
+            <Propaganda reverse={false} title='Giorgio Armani'sr="Giorgio Armani"/>
+            <Propaganda reverse={true} title='Yves Saint Laurent' sr='Yves Saint Laurent'/>
+            <Propaganda reverse={false} title='Lancome' sr='Lancome'/>
+          </S.Section>
         </S.Main>
     </>
 
