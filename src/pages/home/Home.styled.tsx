e8dad1a199174e65a63879cc6c022155971ad39a
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-23 12:57:52 -0300

    components de estilo da pagina principal

--- src/pages/home/Home.styled.tsx
+++ src/pages/home/Home.styled.tsx
@@ -2,5 +2,25 @@ import styled from "styled-components";
 
 
 export const Main = styled.main`
-    
+    width: 100vw;
+    height: calc(100vh - 120px);
+    color: black;
+    background-color: white;
+`
+
+export const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    padding-bottom: 20px;
+`
+
+export const Img = styled.img`
+    border: none;
+    cursor: pointer;
+    width: 150px;
+    min-width: 150px;
+    height: 100%;
 `
