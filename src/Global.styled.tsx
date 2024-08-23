3c55cdb921bd376b2e0c061f24fe4dc2a6805ce8
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-23 12:59:45 -0300

    estilo global

--- src/Global.styled.tsx
+++ src/Global.styled.tsx
@@ -1,10 +1,21 @@
 import {createGlobalStyle} from "styled-components"
 
+
 export const GlobalStyled = createGlobalStyle`
     *{
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+
+    html{
+        overflow-x: hidden;
+        background-color: white;
+        scrollbar-width: none;
+        -ms-overflow-style: none;
+        &::-webkit-scrollbar{
+        display: none;
+        }
     }
 `
