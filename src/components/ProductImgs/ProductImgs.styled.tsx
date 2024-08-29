1e218517d16d1d379add85b535ab89daf5e5b545
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-29 15:28:15 -0300

    components de estilo do component ProductImgs

---
+++ src/components/ProductImgs/ProductImgs.styled.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+export const DivImgs = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 60%;
+
+    @media screen and (max-width: 480px){
+        width: 100%;
+    }
+`
+
+export const Img = styled.img`
+    cursor: pointer;
+    width: 50%;
+    max-width: 400px;
+`
+
+export const ContainerClickedImg = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: #ffffff69;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+`
+
+export const ClickekImg = styled.img`
+    cursor: pointer;
+    height: 90vh;
+`
