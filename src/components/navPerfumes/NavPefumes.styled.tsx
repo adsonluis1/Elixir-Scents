f71dcaf25d0ebb6de5680fcd655bce0bae4ab614
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-28 21:29:43 -0300

    mode dark

--- src/components/navPerfumes/NavPefumes.styled.tsx
+++ src/components/navPerfumes/NavPefumes.styled.tsx
@@ -1,11 +1,21 @@
 import styled from "styled-components";
 
-export const Nav = styled.nav`
+type Tdark = {
+    $dark?:boolean
+}
+
+export const Nav = styled.nav<Tdark>`
     display: flex;
-    align-items: baseline;
-    justify-content: space-between;
-    background-color: black;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    background-color: ${({$dark})=> $dark?'black':'white'};
+    
     padding: 10px 20px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
 
     & a {
         transition: 0.2s;
@@ -18,4 +28,40 @@ export const Nav = styled.nav`
     & a:hover{
         color: white;
     }
+
+    & svg{
+        cursor: pointer;
+        display: block;
+        color: ${({$dark})=> $dark?'white':'black'};
+        font-size: 2em;
+    }
+    
+`
+
+export const CoverImgs = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
+    gap: 10px;
+`
+
+export const DivImg = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width:45%;
+    max-width: 300px;
+    min-width: 120px;
+`
+
+export const Img = styled.img`
+    width: 100%;
+`
+
+export const Description = styled.p<Tdark>`
+    color: ${({$dark})=> $dark?'white':'black'};
+    cursor: default;
+    font-size: 1.3em;
+    font-weight: bold;
 `
