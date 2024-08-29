f73dc5e0050a561a3aeb3886defb8bb75d35b4ba
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-29 15:32:55 -0300

    component de estilo do component productOptions

---
+++ src/components/ProductOptions/ProductOptions.styled.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const DivOptions = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 10px 0px;
+    gap: 10px;
+`
+
+export const Options = styled.input`
+    
+`
+
+export const Label = styled.label`
+    display: flex;
+    gap: 5px;
+`
