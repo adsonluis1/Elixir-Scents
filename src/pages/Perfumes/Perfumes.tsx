f9a138d0c5289e8d3a01d1c96e9f981a06dc8373
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-28 21:34:23 -0300

    add itens disponiveis para compra

--- src/pages/Perfumes/Perfumes.tsx
+++ src/pages/Perfumes/Perfumes.tsx
@@ -1,28 +1,24 @@
-import { useParams } from 'react-router-dom'
 import * as S from './Perfumes.styled'
 import Header from '../../components/header/Header'
-import NavPefumes from '../../components/navPerfumes/NavPefumes'
-
-type Tperfume = {
-    marca:"Chanel" | "Dior" | "GiorgioArmani" | "YvesSaintLaurent" | "Lancome" | undefined
-}
+import ShowItens from '../../components/ShowItens/ShowItens'
+import HeaderMarca from '../../components/HeaderMarca/HeaderMarca'
 
 const Perfumes = () => {
-    const {marca} = useParams<Tperfume>()
-    
   return (
     <>
     <Header />
     <S.Main>
-        <S.Section>
-            <S.DivTopMarca $marca={marca}>
-                <S.Title>{marca}</S.Title>
-            </S.DivTopMarca>
-            <NavPefumes url={marca}/>
-        </S.Section>
-        <S.Section>
-
-        </S.Section>
+        <HeaderMarca dark={false}/>
+        <S.SectionProducts>
+            <ShowItens name='Homme' price={959.99} img='/img/Dior/masculino-600x600.webp' tags={['#novidade', '#Exclusivo']}/>
+            <ShowItens name='Miss Dior' price={839.99} img='/img/Dior/feminino-600x600.webp' tags={['#novidade', '#Exclusivo']}/>
+            <ShowItens name='Homme' price={959.99} img='/img/Dior/masculino-600x600.webp' tags={['#novidade', '#Exclusivo']}/>
+            <ShowItens name='Miss Dior' price={839.99} img='/img/Dior/feminino-600x600.webp' tags={['#novidade', '#Exclusivo']}/>
+            <ShowItens name='Homme' price={959.99} img='/img/Dior/masculino-600x600.webp' tags={['#novidade', '#Exclusivo']}/>
+            <ShowItens name='Miss Dior' price={839.99} img='/img/Dior/feminino-600x600.webp' tags={['#novidade', '#Exclusivo']}/>
+            <ShowItens name='Homme' price={959.99} img='/img/Dior/masculino-600x600.webp' tags={['#novidade', '#Exclusivo']}/>
+            <ShowItens name='Miss Dior' price={839.99} img='/img/Dior/feminino-600x600.webp' tags={['#novidade', '#Exclusivo']}/>
+        </S.SectionProducts>
     </S.Main>
     </>
   )
