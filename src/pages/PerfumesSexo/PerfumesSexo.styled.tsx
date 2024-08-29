a04dc0ba5930c8d8a8abd66dd41a8251cc799239
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-29 15:47:33 -0300

    components de estilo da pagina PerfumesSexo

---
+++ src/pages/PerfumesSexo/PerfumesSexo.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { SectionProducts as SectionP } from "../Perfumes/Perfumes.styled";
+
+export const Main = styled.main`
+    
+`
+
+export const SectionProducts = styled(SectionP)``
+
+export const Img = styled.img`
+    width: 100vw;
+`
