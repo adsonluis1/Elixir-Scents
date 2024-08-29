7e1c8f132c928d224b4033fc8b28850adcaa815c
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-29 15:42:20 -0300

    ...

--- src/pages/home/Home.styled.tsx
+++ src/pages/home/Home.styled.tsx
@@ -8,7 +8,7 @@ export const Main = styled.main`
     background-color: white;
 `
 
-export const Section = styled.section`
+export const SectionPropaganda = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
