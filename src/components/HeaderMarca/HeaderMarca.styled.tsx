d18d8c8299c653ea861763e9ebe57e310d616799
adsonluis1 <dev.adsonluis@gmail.com>
2024-08-28 21:26:06 -0300

    components de estilo do HeaderMarca

---
+++ src/components/HeaderMarca/HeaderMarca.styled.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const DivTopMarca = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100px;
+    background-color: black;
+`
+
+export const Title = styled.h1`
+    color: white;
+    cursor: default;
+    font-size: clamp(2em, 6vh, 3em);
+`
