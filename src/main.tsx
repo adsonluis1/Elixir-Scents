b5f80d56b88b775075a3037fb2500e436c1bee6a
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-05 15:53:47 -0300

    add reducer provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyled } from './Global.styled.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ContextProviber } from './context/Login.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <GlobalStyled />
-      <App />
-    </BrowserRouter>
+    <ContextProviber>
+      <BrowserRouter>
+        <GlobalStyled />
+        <App />
+      </BrowserRouter>
+    </ContextProviber>
   </StrictMode>,
 )
