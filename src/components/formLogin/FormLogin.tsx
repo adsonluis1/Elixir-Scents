6c8b4e744cdaa449d1b3b56f475c603d072ee5d3
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-05 15:30:54 -0300

    mudança nos nomes dos inputs

--- src/components/formLogin/FormLogin.tsx
+++ src/components/formLogin/FormLogin.tsx
@@ -1,10 +1,10 @@
-import useLogin from '../../hooks/useLogin'
+import useFormLogin from '../../hooks/useFormLogin'
 import * as S from './FornLogin.styled'
 import { IoEnterOutline } from "react-icons/io5";
 
 const FormLogin = () => {
-  const {handleSubmit,handleSubmitData,register,errors} = useLogin()
-  
+  const {handleSubmit,register,errors, useLogin} = useFormLogin()
+
   return (
     <S.Section>
         <S.DivTitle>
@@ -12,14 +12,14 @@ const FormLogin = () => {
           <S.SubTitle>Estavamos Esperando você</S.SubTitle>
           <S.Description>Coloque as informações do seu login para entrar na sua conta</S.Description>
         </S.DivTitle>
-        <S.Form onSubmit={handleSubmit(handleSubmitData)}>
-            <S.Input {...register('login')} placeholder='Digite seu Login'/>
-            {errors.login &&
-              <S.Error>{errors.login.message}</S.Error>
+        <S.Form onSubmit={handleSubmit(useLogin)}>
+            <S.Input {...register('email')} placeholder='Digite seu Login'/>
+            {errors.email &&
+              <S.Error>{errors.email.message}</S.Error>
             }
-            <S.Input {...register('senha')} type='password' placeholder='Digite sua senha'/>
-            {errors.senha &&
-              <S.Error>{errors.senha.message}</S.Error>
+            <S.Input {...register('password')} type='password' placeholder='Digite sua senha'/>
+            {errors.password &&
+              <S.Error>{errors.password.message}</S.Error>
             }
             <S.Button>Entrar <IoEnterOutline /></S.Button>
         </S.Form>
