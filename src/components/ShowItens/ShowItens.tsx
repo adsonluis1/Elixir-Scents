d50a8a9d4499afebab861409e61dc8169aed70d6
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-05 15:54:55 -0300

    usando props para pegar as informações

--- src/components/ShowItens/ShowItens.tsx
+++ src/components/ShowItens/ShowItens.tsx
@@ -2,27 +2,31 @@ import { NavLink, useParams } from 'react-router-dom'
 import * as S from './ShowItens.styled'
 import { TMarcas } from '../../pages/PerfumesSexo/PerfumesSexo'
 
-type TProduct = {
+type TProfileImage = {
+  path:string
+}
+
+export type TProduct = {
     product:{
       name:string
       price:number
       tags:string[]
-      img:string
-      id:number | string
+      profileImage:TProfileImage
+      _id:number | string
     }
 }
 
 const ShowItens = ({product}:TProduct) => {
   const {marca} = useParams<TMarcas>()
-  const {id, img, name, price,tags} = product
+  const {_id, profileImage, name, price,tags} = product
   return (
     <S.Div>
-        <NavLink to={`/${marca}/${id}`}>
-          <S.Img src={img} alt={`foto do perfume ${name}`}/>
+        <NavLink to={`/${marca}/${_id}`}>
+          <S.Img src={profileImage.path} alt={`foto do perfume ${name}`}/>
         </NavLink>
         <S.DivTags>
             {tags.map((tag)=>[
-                <S.Tag>{tag}</S.Tag>
+                <S.Tag>#{tag}</S.Tag>
             ])}
         </S.DivTags>
         <S.Title>{name}</S.Title>
