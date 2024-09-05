b961ab0be2e3a37fe08ea44120804fc2ffe4487f
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-05 15:48:59 -0300

    pegando os produtos pela api e fazendo caminho para o erro e para o load

--- src/pages/Perfumes/Perfumes.tsx
+++ src/pages/Perfumes/Perfumes.tsx
@@ -2,77 +2,32 @@ import * as S from './Perfumes.styled'
 import Header from '../../components/header/Header'
 import ShowItens from '../../components/ShowItens/ShowItens'
 import HeaderMarca from '../../components/HeaderMarca/HeaderMarca'
+import useGetProductsLabel from '../../hooks/useGetProductsLabel'
 
 const Perfumes = () => {
-  const products = [
-    {
-      id:1, 
-      name:'Homme', 
-      price:959.99,
-      img:'/img/Dior/masculino-600x600.webp', 
-      tags:['#novidade', '#Exclusivo']
-    },
-    {
-      id:2, 
-      name:'Homme', 
-      price:959.99,
-      img:'/img/Dior/feminino-600x600.webp', 
-      tags:['#novidade', '#Exclusivo']
-    },
-    {
-      id:3, 
-      name:'Homme', 
-      price:959.99,
-      img:'/img/Dior/masculino-600x600.webp', 
-      tags:['#novidade', '#Exclusivo']
-    },
-    {
-      id:4, 
-      name:'Homme', 
-      price:959.99,
-      img:'/img/Dior/feminino-600x600.webp', 
-      tags:['#novidade', '#Exclusivo']
-    },
-    {
-      id:5, 
-      name:'Homme', 
-      price:959.99,
-      img:'/img/Dior/masculino-600x600.webp', 
-      tags:['#novidade', '#Exclusivo']
-    },
-    {
-      id:6, 
-      name:'Homme', 
-      price:959.99,
-      img:'/img/Dior/feminino-600x600.webp', 
-      tags:['#novidade', '#Exclusivo']
-    },
-    {
-      id:7, 
-      name:'Homme', 
-      price:959.99,
-      img:'/img/Dior/masculino-600x600.webp', 
-      tags:['#novidade', '#Exclusivo']
-    },
-    {
-      id:8, 
-      name:'Homme', 
-      price:959.99,
-      img:'/img/Dior/feminino-600x600.webp', 
-      tags:['#novidade', '#Exclusivo']
-    }
-  ]
-
+  const {products, load, error} = useGetProductsLabel('Chanel')
   return (
     <>
     <Header />
     <S.Main>
         <HeaderMarca dark={false}/>
-        <S.SectionProducts>
-            {products.map((product,index)=>[
-              <ShowItens key={index} product={product}/>
-            ])}
-        </S.SectionProducts>
+        {products && 
+          <S.SectionProducts>
+              {products.map((product,index)=>[
+                <ShowItens key={index} product={product}/>
+              ])}
+          </S.SectionProducts>
+        }
+
+        {load && !error ? 
+        <S.Loader>Carregando...</S.Loader>
+        :""
+        }
+
+        {!load && error ? 
+        <S.Error>Error ao conectar com o servidor</S.Error>
+        :"" 
+      }
     </S.Main>
     </>
   )
