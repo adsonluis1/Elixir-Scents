b64bdd67a036fd27eae3685f419ddabf63074eef
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-05 15:52:19 -0300

    pegando informações do produto pela api e fazendo caminho de error e de load

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -5,35 +5,43 @@ import { useParams } from 'react-router-dom'
 import ProductDescription from '../../components/ProductDescripition/ProductDescription'
 import ProductImgs from '../../components/ProductImgs/ProductImgs'
 import ProductOptions from '../../components/ProductOptions/ProductOptions'
+import useGetProductById from '../../hooks/useGetProductById'
+import ProductMaisDetalhes from '../../components/ProductMaisDetalhes/ProductMaisDetalhes'
 
 type TIdAndMarcas = {
     marca:string,
     id:string
 }
 const Product = () => {
-    const {id} = useParams<TIdAndMarcas>()
-    console.log(id)
-
+    const {id,marca} = useParams<TIdAndMarcas>()
+    const {product,error,load} = useGetProductById(id!,marca!)
+    
     return (
     <>
         <Header />
         <HeaderMarca dark={true}/>
+        {product &&
         <S.Main>
             <S.Section>
-                {/* enviar as informações por props */}
-                <ProductImgs />
+                <ProductImgs url={product.profileImage.path}/>
                 <S.DivRight>
-                    {/* enviar as informações por props */}
-                    <ProductDescription />
-                    {/* enviar as informações por props */}
+                    <ProductDescription name={product.name} price={product.price} tags={product.tags} key={product._id}/>
                     <ProductOptions />
                 </S.DivRight>
             </S.Section>
 
             <S.Section id='detalhes'>
-                
+                <ProductMaisDetalhes aroma={product.aroma} description={product.description}/>
             </S.Section>
         </S.Main>
+        }
+
+        {load && 
+            <S.Loader>Carregando...</S.Loader>
+        }
+        {error && 
+            <S.Error>Error ao carregar a página</S.Error>
+        }
     </>
   )
 }
