36494cc0843b194c89c806ffd54e1da35b0f61aa
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-06 15:04:40 -0300

    funções importantes da aplicação

---
+++ src/utils/functions.ts
@@ -0,0 +1,15 @@
+import { NavigateFunction } from "react-router-dom"
+import { IProduct } from "../types/Product"
+import { IUser } from "../types/User"
+import useAddProductIntoBag from "../hooks/useAddProductIntoBag"
+import React, { SetStateAction } from "react"
+
+
+export const handleClickBuyButton = (user:IUser,product:IProduct, navigate:NavigateFunction,setDone:React.Dispatch<SetStateAction<boolean>>)=>{
+    if(!user?.cpf){
+        navigate('/login')
+        return
+    }
+    console.log(product)
+    useAddProductIntoBag(user,product,setDone)
+}
