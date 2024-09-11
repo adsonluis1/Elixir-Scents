78fc7a34700fc80cc00f38586f1dbb278523ed8f
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-10 22:33:11 -0300

    components de estilo do component de formulario edit address

---
+++ src/components/formEditAddress/FormEditAddress.styled.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    background-color: transparent;
+    height: 80%;
+    color: white;
+    width: 50%;
+    padding: 5px;
+
+    @media screen and (max-width:480px){
+        width: 100%;
+    }
+`
+
+export const DivTitle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: baseline;
+    height: 20%;
+    gap: 10px;
+`
+
+export const Title = styled.h1`
+    font-size: clamp(2em, 4vw, 4em);
+    text-align: justify;
+`
+
+export const Description = styled.p`
+    color: #505050;
+    text-align: center;
+    margin-top: 10px;
+    font-size: 1.2em;
+
+    @media screen and (max-width:480px){
+        color: #858585;
+    }
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin-top: 20px;
+    gap: 20px;
+`
+
+export const Input = styled.input`
+    padding: 8px;
+    border-radius: 4px;
+    border: 1px solid grey;
+    width: clamp(200px, 80%, 400px);
+`
+
+export const Button = styled.button`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    background-color: transparent;
+    color: white;
+    border: 1px solid white;
+    font-size: 1.3em;
+    transition: .2s;
+    gap: 3px;
+    padding:8px 16px;
+    border-radius: 8px;
+
+    &:hover{
+        background-color: white;
+        color: black;
+    }
+`
+
+export const Error = styled.p`
+    color: red;
+    text-align: center;
+    font-size: 0.9em;
+`
