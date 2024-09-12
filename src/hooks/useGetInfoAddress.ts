0b57977bc2e849b18f4580257f6c44ed16dfdf4c
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-12 06:26:40 -0300

    hook que pega as informações do endereço pelo cep

--- src/hooks/useGetInfoAddress.ts
+++ src/hooks/useGetInfoAddress.ts
@@ -15,7 +15,6 @@ const useGetInfoAddress = (cep:string,setValue:UseFormSetValue<TEditAddress>) =>
             setValue('city',dataInfo.localidade)
             setValue('neighborhood',dataInfo.bairro)
             setValue('street',dataInfo.logradouro)
-            console.log(dataInfo)
             data = dataInfo
         } catch (err) {
             error = err
