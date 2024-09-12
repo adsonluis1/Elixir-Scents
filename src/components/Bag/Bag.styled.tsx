39d1a38d5a417b159a5168c5522d64ed54ed3fef
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-12 06:17:22 -0300

    posicionando icon de fechar component

--- src/components/Bag/Bag.styled.tsx
+++ src/components/Bag/Bag.styled.tsx
@@ -21,6 +21,7 @@ export const DivBag = styled.div<TShowOrNotBag>`
     align-items: center;
     position: absolute;
     background-color: white;
+    gap: 5px;
     z-index: 1;
     padding: 10px;
     width: 60vw;
@@ -38,7 +39,8 @@ export const DivBag = styled.div<TShowOrNotBag>`
         & > .close {
         display: block;
         position: absolute;
-        right: 10px;
+        top: 5px;
+        right: 5px;
         font-size: 1.5em;
     }
     }
@@ -46,51 +48,11 @@ export const DivBag = styled.div<TShowOrNotBag>`
 
 export const TitleBag = styled.h2`
     font-size: 2em;
-    margin: 50px 0px;
+    margin-top: 15px;
 `
 
-export const DivCompra = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-    width: 100%;
-    height: 120px;
+export const Error = styled.p`
+    font-size: 1.3em;
 `
 
-
-export const Img = styled.img`
-    height: 100%;
-`
-export const DivInfoProduto = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-`
-
-export const TitleCompra = styled.p`
-    font-size: 1.2em;
-    font-weight: bold;
-`
-
-export const ProductDescription = styled.p`
-    color: #505050;
-`
-
-export const DivInfoCompra = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-`
-
-export const Quantidade = styled.p`
-    color: #505050;
-`
-
-export const Price = styled.p`
-    font-weight: bold;
-`
-
-export const Button = styled.button`
-    
-`
+export const Loader = styled(Error)``
