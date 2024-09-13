186610146c76a413ed75285ae2886d58dc0f4a2d
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-12 22:49:45 -0300

    removendo alguns estilos que viraram components

---
+++ src/pages/purchaseHistory/PurchaseHistory.styled.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 10px; 
+    gap: 30px;
+`
+
+export const Title = styled.h1`
+    font-size: clamp(1.5em, 4vw, 2.5em);
+    margin-top: 10px;
+`
+
