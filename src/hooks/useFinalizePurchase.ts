bf403dc497af5c4d7885f4cb972133c79ef35ea2
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-12 22:45:59 -0300

    mudando type do argumento da função

--- src/hooks/useFinalizePurchase.ts
+++ src/hooks/useFinalizePurchase.ts
@@ -1,13 +1,29 @@
 import { IUser } from "../types/User"
 
-const useFinalizePurchase = async (idProduct:string | string,labelProduct:string,user:IUser,amount:number) => {
-    await fetch('http://localhost:3333/account/buy',{
-        method:"POST",
-        headers:{
-            "Content-Type": "application/json"
+const useFinalizePurchase = async (idProduct:string[]|string,labelProduct:string[]|string,user:IUser,amount:number[]|number) => {
+    const product = {
+        idProduct,
+        labelProduct,
+        user:{
+            _id:user._id,
+            cpf:user.cpf,
+            address:user.address
         },
-        body:JSON.stringify({idProduct,labelProduct,user,amount})
-    })
+        amount
+    }
+
+    console.log(product)
+    try {
+        await fetch('http://localhost:3333/account/buy',{
+            method:'POST',
+            body:JSON.stringify(product),
+            headers:{
+                "Content-Type": "application/json"
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export default useFinalizePurchase
