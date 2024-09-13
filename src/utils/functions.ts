2c2c4cadb0a574d5cc484b9e3799ac5b2cf1648a
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-12 22:52:17 -0300

    add funções para limpar a sacola e finalizar a compra

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -3,6 +3,8 @@ import { IProduct } from "../types/Product"
 import { IUser } from "../types/User"
 import useAddProductIntoBag from "../hooks/useAddProductIntoBag"
 import React, { SetStateAction } from "react"
+import useFinalizePurchase from "../hooks/useFinalizePurchase"
+import useRemoveProductByBag from "../hooks/useRemoveProductByBag"
 
 
 export const handleClickBuyButton = (user:IUser,product:IProduct, navigate:NavigateFunction,setDone:React.Dispatch<SetStateAction<boolean>>)=>{
@@ -12,4 +14,30 @@ export const handleClickBuyButton = (user:IUser,product:IProduct, navigate:Navig
     }
 
     useAddProductIntoBag(user,product,setDone)
+}
+
+export const handleClickSelectProductFinishBuy = (idProduct:string,labelProduct:string, user:IUser,amount:number,setSelectProduct:React.Dispatch<SetStateAction<IProduct | null>>)=>{
+    useFinalizePurchase(idProduct,labelProduct,user,amount)
+    useRemoveProductByBag(user._id,idProduct)
+    setSelectProduct(null)
+}
+
+export const handleClickSelectProductClearBag = (idProduct:string,user:IUser, setSelectProduct:React.Dispatch<SetStateAction<IProduct | null>>)=>{
+    useRemoveProductByBag(user._id, idProduct)
+    setSelectProduct(null)
+}
+
+export const handleClickProductFinishBuy = (products:IProduct[], user:IUser, setBag:React.Dispatch<SetStateAction<IProduct[]>>)=>{
+    const idProducts = products.map((product) => product._id)
+    const amountProducts = products.map((product) => product.amount)
+    const labelproduct = products.map((product) => product.label)
+    useFinalizePurchase(idProducts, labelproduct,user,amountProducts)
+    useRemoveProductByBag(user._id,idProducts)
+    setBag([])
+}
+
+export const handleClickProductClearBag = (bag:IProduct[],user:IUser, setBag:React.Dispatch<SetStateAction<IProduct[]>>)=>{
+    const idProducts = bag.map((product) => product._id)
+    useRemoveProductByBag(user._id, idProducts)
+    setBag([])
 }
