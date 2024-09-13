90b58144ff3cc06f9f87a6c08c73118d97b2f9ad
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-12 22:50:48 -0300

    pagina para mostrar produtos comprados

--- src/pages/purchaseHistory/PurchaseHistory.tsx
+++ src/pages/purchaseHistory/PurchaseHistory.tsx
@@ -1,8 +1,27 @@
 import * as S from './PurchaseHistory.styled'
+import { IUser } from '../../types/User'
+import Header from '../../components/header/Header'
+import useGetPurchaseHistory from '../../hooks/useGetPurchaseHistory'
+import ShowPurchaseProducts from '../../components/showPurchaseProducts/ShowPurchaseProducts'
 
-const PurchaseHistory = () => {
-  return (
-    <div>PurchaseHistory</div>
+const PurchaseHistory = ({user}:{user:IUser}) => {
+  const {products, load, error} = useGetPurchaseHistory(user._id)
+  console.log(error)
+  console.log(load)
+  return (  
+    <>
+        <Header />
+        <S.Main>
+          <S.Section>
+            <S.Title>Historico de compras</S.Title>
+            {products.length > 0 &&
+              products.map((product)=>[
+                <ShowPurchaseProducts key={product.datePurchase} previousPurchases={product}/>
+              ])
+            }
+          </S.Section>
+        </S.Main>
+    </>
   )
 }
 
