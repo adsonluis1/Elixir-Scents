9c5ac685fe7640b648493e1f8b4f59f10f295b71
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-18 12:29:51 -0300

    add overflow no main

--- src/hooks/useDesableScroll.ts
+++ src/hooks/useDesableScroll.ts
@@ -2,13 +2,14 @@ import { useEffect } from "react"
 
 const useDesableScroll = () => {
     useEffect(()=>{
+        const main = document.querySelector('main')
         const desabilitScroll = (evt:Event)=>{
             evt.preventDefault()
         }
-        document.body.style.overflow = "hidden"
+        main!.style.overflow = "hidden"
         window.addEventListener('wheel', desabilitScroll, { passive: false });
         return () => {
-            document.body.style.overflow = ''
+            main!.style.overflow = ''
             window.removeEventListener('wheel', desabilitScroll);
         }
     },[])
