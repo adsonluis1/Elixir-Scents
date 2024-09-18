3234dfaf670e336eaf046e3658e088725b7a0eea
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-18 12:33:11 -0300

    add rota da pagina de produtos pesquisados

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { useContext } from "react"
 import { Context } from "./context/Login"
 import ClientPerfil from "./pages/clientPerfil/ClientPerfil"
 import PurchaseHistory from "./pages/purchaseHistory/PurchaseHistory"
+import ProductSearch from "./pages/ProductSearch/ProductSearch"
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
         <Route path="/dashbordAccount" element={<DashbordLoginRegister />}/>
+        <Route path="/search/:productSearch" element={<ProductSearch />}/> 
         <Route path="/:marca" element={<Perfumes />}/>
         <Route path="/:marca/masculino" element={<PerfumesSexo />}/>
         <Route path="/:marca/feminino" element={<PerfumesSexo />}/>
