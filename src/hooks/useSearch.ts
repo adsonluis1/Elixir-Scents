d1158d88fc9e7b5665c72d6e5c050aaf183e15c4
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-18 12:30:56 -0300

    hook que faz a busca do produto que esta sendo pesquisado

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,29 +1,27 @@
-import { useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod"
-import * as z from 'zod'
+import { useEffect, useState } from "react"
+import { IProduct } from "../types/Product"
 
-const schema = z.object({
-    search: z.string().max(255,'Pesquisa muito grande')
-})
+const useSearch = (productSearch:string) => {
+    const [products, setProducts] = useState<IProduct[]>()
+    const [error, setError] = useState("")
+    const [load, setLoad] = useState(true)
+    useEffect(()=>{
+        const search = async ()=>{
+            try {
+                const dataJson = await fetch(`http://localhost:3333/account/search/${productSearch}`)
+                const data = await dataJson.json()
+                setProducts(data.foundProducts)
+            } catch (error) {
+                if(error instanceof Error)
+                    setError(error.message)
+            }finally{
+                setLoad(false)
+            }
+        }
 
-type InputSearch = z.infer<typeof schema>
-
-const useSearch = ()=>{
-    const {register,handleSubmit,formState:{errors}} = useForm<InputSearch>({
-        mode:'all',
-        defaultValues:{
-          search:''
-        },
-        resolver: zodResolver(schema)
-      })
-      
-      const handleSubmiteData = (data:InputSearch)=> {
-        // fazer a pesquisa aqui
-        console.log(data)
-      }
-
-      return {register,handleSubmit,handleSubmiteData,errors}
+        search()
+    },[productSearch])
+    return {products,load,error}
 }
 
 export default useSearch
-  
